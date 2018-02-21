458d8ccfd230c6e64b1efeb85c0389b048339dd3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-21 12:01:42 -0500

    Orchid Template 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -8,8 +8,8 @@
                  <div id="newsletter_form">
                     <label for="fieldEmail" style="display:none"></label>
                     <input id="fieldEmail" name="cm-tkyhii-tkyhii" class="form-control" type="email" v-model="newsletter_email" :placeholder="$t('footer.enter_email')" required/> 
-                    <router-link class="newsletter_btn" data-i18n="general.submit" :to="'/newsletter?email='+ newsletter_email">
-                        {{$t("footer.subscribe")}}
+                    <router-link data-i18n="general.submit" :to="'/newsletter?email='+ newsletter_email">
+                        <span class="newsletter_btn">{{$t("footer.subscribe")}}</span>
                     </router-link>
                     
                     <span id="success_subscribe" class="hidden_now">{{$t("footer.subscribe_thankyou")}}</span>
