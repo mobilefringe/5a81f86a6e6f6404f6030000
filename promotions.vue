4fb0cdd0771133203b78566889bba300124d9654
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-22 14:25:46 -0500

    Orchid Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -11,7 +11,7 @@
 		<div class="site_container page_content">
 		    <div class="row">
 		        <div class="col-md-12">
-		            <h3>{{$t("promos.page_title")}}</h3>
+		            <h3>{{$t("promos_page.promotions")}}</h3>
 		        </div>
 		    </div>
 			<div id="events_container" v-if="promotions.length > 0">
