e1987fcb3878a61404091ed327c2a68460f849bc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-22 13:21:45 -0500

    Orchid Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -11,6 +11,7 @@
     
 					<div class="details_store_image">
 						<img v-lazy="currentStore.store_front_url_abs" class="image"/>
+						<p v-if="currentStore.phone" class="details_store_phone">{{ currentStore.phone }}</p>
 					</div>
     				<div class="details_store_desc">
     				    <div v-html="currentStore.description"></div>
