63bde37b1622201ee01f0d44dd7ba930ff0dbcf4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-22 16:09:37 -0500

    Orchid Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -19,44 +19,50 @@
 					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }">
 					    <div v-lazy:background-image="promo.image_url"></div>
 					    <div class="promo_content">
-					        
-					    </div>
-						<div class="col-sm-6 col-md-4 event_image_container">
-							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more">
-								<img v-lazy="promo.store.image_url"  class="event_image image" alt=""/>
-							</router-link>
-						</div>
-						<div class="col-sm-6 col-md-8 event_dets_container">
-							<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>
+					        <h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>-->
 							<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>
-							<div v-if="promo.promotionable_type == 'Store'">
-							    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>
-							    <h4 class="event_store_name caps" v-else>{{promo.store.name_2}}</h4>
-							</div>
-							
-							<div class="event_thick_line"></div>
-							<p class="event_dates">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
-							<p class="event_desc"  v-if="locale=='en-ca'" >{{promo.description_short}}</p>
+					        <p class="event_desc"  v-if="locale=='en-ca'" >{{promo.description_short}}</p>-->
 							<p class="event_desc" v-else>{{promo.description_short_2}}</p>
-						
-							<div class="text-right  col-sm-6" v-if="promo" style="padding:0">
-								<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more pull-left">
+							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more pull-left">-->
 								   {{$t("promos_page.read_more")}} <i class="fa fa-angle-right" aria-hidden="true"></i>
-							    </router-link>
-								<social-sharing :url="shareURL(promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="EastgateSquare" :media="promo.image_url" inline-template >
-									<div class="blog-social-share pull_right">
-										<div class="social_share">
-											<network network="facebook">
-												<i class="fa fa-facebook social_icons" aria-hidden="true"></i>
-											</network>
-											<network network="twitter">
-												<i class="fa fa-twitter social_icons" aria-hidden="true"></i>
-											</network>
-										</div>
-									</div>
-								</social-sharing>
-							</div>
-						</div>
+						    </router-link>
+					    </div>
+						<!--<div class="col-sm-6 col-md-4 event_image_container">-->
+						<!--	<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more">-->
+						<!--		<img v-lazy="promo.store.image_url"  class="event_image image" alt=""/>-->
+						<!--	</router-link>-->
+						<!--</div>-->
+						<!--<div class="col-sm-6 col-md-8 event_dets_container">-->
+						<!--	<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>-->
+						<!--	<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>-->
+						<!--	<div v-if="promo.promotionable_type == 'Store'">-->
+						<!--	    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>-->
+						<!--	    <h4 class="event_store_name caps" v-else>{{promo.store.name_2}}</h4>-->
+						<!--	</div>-->
+							
+						<!--	<div class="event_thick_line"></div>-->
+						<!--	<p class="event_dates">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>-->
+						<!--	<p class="event_desc"  v-if="locale=='en-ca'" >{{promo.description_short}}</p>-->
+						<!--	<p class="event_desc" v-else>{{promo.description_short_2}}</p>-->
+						
+						<!--	<div class="text-right  col-sm-6" v-if="promo" style="padding:0">-->
+						<!--		<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more pull-left">-->
+						<!--		   {{$t("promos_page.read_more")}} <i class="fa fa-angle-right" aria-hidden="true"></i>-->
+						<!--	    </router-link>-->
+						<!--		<social-sharing :url="shareURL(promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="EastgateSquare" :media="promo.image_url" inline-template >-->
+						<!--			<div class="blog-social-share pull_right">-->
+						<!--				<div class="social_share">-->
+						<!--					<network network="facebook">-->
+						<!--						<i class="fa fa-facebook social_icons" aria-hidden="true"></i>-->
+						<!--					</network>-->
+						<!--					<network network="twitter">-->
+						<!--						<i class="fa fa-twitter social_icons" aria-hidden="true"></i>-->
+						<!--					</network>-->
+						<!--				</div>-->
+						<!--			</div>-->
+						<!--		</social-sharing>-->
+						<!--	</div>-->
+						<!--</div>-->
 						<div class="col-sm-12">
 							<hr>
 						</div>
