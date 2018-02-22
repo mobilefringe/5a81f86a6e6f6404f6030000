63657b3c9eec317eacb6a2164ea2a8ba778c554d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-22 14:29:08 -0500

    Orchid Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -14,9 +14,13 @@
 		            <h3 class="center">{{$t("promos_page.promotions")}}</h3>
 		        </div>
 		    </div>
-			<div id="events_container" v-if="promotions.length > 0">
+			<div id="promos_container" v-if="promotions.length > 0">
 				<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="4">
 					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }">
+					    <div v-lazy:background-image="promo.image_url">
+					        
+					    </div>
+					    
 						<div class="col-sm-6 col-md-4 event_image_container">
 							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more">
 								<img v-lazy="promo.store.image_url"  class="event_image image" alt=""/>
