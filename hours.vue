f30bfce32f5e67d7a77a3252a336125aad1f633f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-23 09:11:45 -0500

    Orchid Template 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -15,7 +15,7 @@
                 <div class="site_container">
                     <div class="all_hours_container">
                         <div class="row">
-                            <div class="col-sm-12">
+                            <div class="col-sm-6">
                                 <h3 class="hours_heading text-left" >{{$t("hours_page.reg_hours")}}</h3>
                                 <div id="hours_container" class="hours_container">
                                     <div class="hours_div text-left" v-for="hour in hours">
@@ -26,7 +26,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-sm-12 ">
+                            <div class="col-sm-6">
                                 <div class="padding_top_20"></div>
                                 
                                 <h3 class="hours_heading text-left">{{$t("hours_page.extended_holiday_hours")}}</h3>
