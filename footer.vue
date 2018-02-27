6ca250c124d79ea4b85996bda4fead17cbdb7cc3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-27 11:10:55 -0500

    Orchid Template 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -4,7 +4,7 @@
             <div class="newsletter_content_container">
                  <h3 class="caps">{{$t("footer.newsletter")}}</h3>
                  <h1 class="caps">{{$t("footer.subscription")}}</h1>
-                 <h3 class="caps">{{$t("footer.newsletter_desc")}}</h3>
+                 <h3 class="newsletter_desc caps">{{$t("footer.newsletter_desc")}}</h3>
                  <div id="newsletter_form">
                     <label for="fieldEmail" style="display:none"></label>
                     <input id="fieldEmail" name="cm-tkyhii-tkyhii" class="form-control" type="email" v-model="newsletter_email" :placeholder="$t('footer.enter_email')" required/> 
