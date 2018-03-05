8d5cdd1f7f2ab2c25bbe57e8a7e5a1a48f5ad332
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-05 17:29:50 -0500

    Orchid Template 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -8,7 +8,7 @@
         			<div id="promos_container">
     					<div class="promo_container">
     					    <div class="promo_content center">
-    					        <p class="promo_title">{{ $t("promos_page.promotions") }}</p>
+    					        <p class="promo_title">{{ $t("events_page.promotions") }}</p>
     					        <h3 class="margin_60" v-if="locale=='en-ca'">{{ currentEvent.name }}</h3>
     							<h3 class="margin_60" v-else>{{ currentEvent.name_2 }}</h3>
     							<p class="promo_desc">
@@ -51,12 +51,6 @@
             created(){
                 this.loadData().then(response => {
                     this.updatecurrentEvent(this.id);
-                    // var temp_repo = this.findRepoByName('Events Banner');
-                    // if(temp_repo) {
-                    //     this.eventBanner = temp_repo.images[0];
-                    // }
-                    // console.log(this.eventBanner);
-                    // this.events = this.event;
                     this.dataLoaded = true;
                 });
             },
@@ -64,55 +58,25 @@
                 currentEvent : function (){
                     if(this.currentEvent != null) {
                         console.log(this.currentEvent.store);
-                        if (this.currentEvent.store != null && this.currentEvent.store != undefined && _.includes(this.currentEvent.store.image_url, 'missing')) {
-                            this.currentEvent.store.image_url = "http://via.placeholder.com/400x400/757575";
-                        }
-                        else if (this.currentEvent.store == null || this.currentEvent.store == undefined) {
+                        if (this.currentEvent.store != null && this.currentEvent.store != undefined) {
+                            if(_.includes(this.currentEvent.store.image_url, 'missing')) {
+                                this.currentEvent.store.image_url = "http://via.placeholder.com/400x400/757575";
+                            }
+                        } else if (this.currentEvent.store == null || this.currentEvent.store == undefined) {
                             this.currentEvent.store = {};
                             this.currentEvent.store.image_url =  "http://via.placeholder.com/400x400/757575";
                         }
-                        var vm = this;
-                        var temp_event = [];
-                        var current_id =_.toNumber(this.currentEvent.id);
-                        _.forEach(this.currentEvent.store.event, function(value, key) {
-                            if(_.toNumber(value) != current_id){
-                                var current_event = vm.findEventById(value);
-                                current_event.description_short = _.truncate(current_event.description, {'length': 70});
-                                temp_event.push(current_event);
-                            }
-                        });
-                        this.storeEvents = temp_event;
-                    }
-                    if(this.currentEvent.store) {
-                        var storeHours = [];
-                        var vm = this;
-                        _.forEach(this.currentEvent.store.store_hours, function (value, key) {
-                            var hour = vm.findHourById(value);
-                            if(hour.day_of_week === 0){
-                                hour.order = 7;
-                            }
-                            else {
-                                hour.order = hour.day_of_week;
-                            }
-                            storeHours.push();
-                        });
-                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);;
                     }
                 }
             },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
+                    'timezone',
                     'processedEents',
                     'findEventBySlug',
-                    'findEventById',
-                    'timezone',
-                    'findRepoByName',
-                    'findHourById'
-                ]),
-                allEvents() {
-                    return this.processedEvents;
-                },
+                    'findEventById'
+                ])
             },
             methods: {
                 updatecurrentEvent (id) {
@@ -128,11 +92,7 @@
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
-                },
-                shareURL(slug){
-                    var share_url = "http://mallmaverick.ca/event/" + slug;
-                    return share_url;
-                },
+                }
             }
         });
     });
