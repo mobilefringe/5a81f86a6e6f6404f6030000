4001582e53dc90ff6f75278abc90612beb0d8d60
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-05 16:41:08 -0500

    Orchid Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -108,11 +108,11 @@
             created(){
                 this.loadData().then(response => {
                     this.updateCurrentPromo(this.id);
-                    var temp_repo = this.findRepoByName('Promos Banner');
-                    if(temp_repo) {
-                        this.promoBanner = temp_repo.images[0];
-                    }
-                    console.log(this.promoBanner);
+                    // var temp_repo = this.findRepoByName('Promos Banner');
+                    // if(temp_repo) {
+                    //     this.promoBanner = temp_repo.images[0];
+                    // }
+                    // console.log(this.promoBanner);
                     this.promos = this.promotions;
                 });
             },
