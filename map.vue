51ac49f5babe2bff6886596b5a67ac02185a66cc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-06 14:44:07 -0500

    Orchid Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -57,7 +57,9 @@
                 return {
                     dataLoaded: false,
                     pageBanner: null,
-                    
+                    alphabet : [
+                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                    ],
                     suggestionAttribute: "name",
                     storeSearch: null,
                     currentSelection: null,
