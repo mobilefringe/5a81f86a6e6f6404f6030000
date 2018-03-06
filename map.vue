fa4cb7c643e42385a9cc0c464f2772c815939231
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-06 15:09:17 -0500

    Orchid Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -162,6 +162,7 @@
                     if(letter == "All" || letter == undefined || letter == null){
                         this.filteredStores = this.processedStores;
                     } else {
+                        console.log(this.storesByAlphaIndex)
                         var filtered = _.filter(this.storesByAlphaIndex, function(o,i) { return _.lowerCase(i) == _.lowerCase(letter); })[0];
                         this.filteredStores = _.groupBy(filtered, store => (isNaN(store.name.charAt(0)) ? store.name.charAt(0) : "#"));
                     }
