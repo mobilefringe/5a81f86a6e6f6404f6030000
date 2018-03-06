62fa35102eba23509896104ea97ec5f01348a64e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-06 13:15:28 -0500

    Orchid Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -56,10 +56,10 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Map Banner');
-                    if(temp_repo) {
-                        this.pageBanner = temp_repo.images[0];
-                    }
+                    // var temp_repo = this.findRepoByName('Map Banner');
+                    // if(temp_repo) {
+                    //     this.pageBanner = temp_repo.images[0];
+                    // }
                     
                     this.currentSelection = this.allStores;
                     
