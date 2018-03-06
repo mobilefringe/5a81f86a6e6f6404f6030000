43c175dac8ed0eee7a9d9437cbabc4ea0b23eedf
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-06 16:54:40 -0500

    Orchid Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -6,7 +6,7 @@
 					<div class="" v-for="banner in banners" v-if="banners">
 						<!--<img :src="banner.image_url" class="banner_img" alt="">-->
 						<div class="home_banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
-						    <div v-if="banner.name && banner.description">
+						    <div class="home_banner_content" v-if="banner.name && banner.description">
 						        <p>{{ banner.name }}</p>
 						        <p>{{ banner.description }}</p>
 						    </div>   
