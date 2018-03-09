f8a48c4a2059163a75f7aa7399d3bbb30d4faedb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-09 10:58:38 -0500

    Orchid Template 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -3,56 +3,45 @@
         <loader v-if="!dataLoaded"></loader>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-        		<div class="page_header hidden_phone" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-        			<div class="site_container">
-        				<div class="header_content caps">
-        				    <p>{{ $t("jobs_page.jobs_header_desc") }} {{ property.name }}</p>
-        					<h1>{{ $t("jobs_page.jobs") }}</h1>
-        				</div>
-        			</div>
-        		</div>
         		<div class="site_container">
-                    <div class="row"> 
-                        <div class="col-md-12 contact_contents">
-                            <form class="form-horizontal" action="https://mobilefringe.createsend.com/t/d/s/bidirr/" method="post" @submit.prevent="validateBeforeSubmit">
-                                <div class="form-group ">
-                                    <div class="col-sm-6 col-xs-12" >
-                                        <label class="label" for="cm-name">{{$t("newsletter_page.name")}}</label>
-                                        <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name">
-                                    </div>
+                    <div class="contact_container">
+                        <form class="form-horizontal" action="https://mobilefringe.createsend.com/t/d/s/bidirr/" method="post" @submit.prevent="validateBeforeSubmit">
+                            <div class="form-group ">
+                                <div class="col-sm-6 col-xs-12" >
+                                    <label class="label" for="cm-name">{{$t("newsletter_page.name")}}</label>
+                                    <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name">
                                 </div>
-                                <div class="form-group">
-                                    <div class="col-sm-6 col-xs-12">
-                                        <label class="label" for="cm-irudui-irudui">{{$t("newsletter_page.email")}}</label>
-                                        <input v-model="form_data.email" required class="form-control" name="cm-bidirr-bidirr" type="email" placeholder="Email" id="newsletter_email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-            					        <label class="checkbox">
-                                            <input name="agree_newsletter" required  type="checkbox">
-                                                {{$t("newsletter_page.agree")}} {{property.name}}. 
-                                        </label>
-            					    </div>
-            					</div>
-            					<div class="form-group">
-                                    <div class="col-xs-12">
-                                        <button class="contest_btn" type="submit" :disabled="formSuccess">{{$t("newsletter_page.subscribe")}}</button>
-                                    </div>
-                                </div>
-                            </form>
-                            
-                            <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
-                                <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                                <span class="sr-only">{{$t("newsletter_page.success")}} : </span>
-                                {{$t("newsletter_page.thank_you_message")}}
                             </div>
-                            <div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">
-                                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                <span class="sr-only">{{$t("newsletter_page.error")}} : </span>
-                                {{$t("newsletter_page.error_message")}}
+                            <div class="form-group">
+                                <div class="col-sm-6 col-xs-12">
+                                    <label class="label" for="cm-irudui-irudui">{{$t("newsletter_page.email")}}</label>
+                                    <input v-model="form_data.email" required class="form-control" name="cm-bidirr-bidirr" type="email" placeholder="Email" id="newsletter_email">
+                                </div>
                             </div>
+                            <div class="form-group">
+                                <div class="col-xs-12">
+        					        <label class="checkbox">
+                                        <input name="agree_newsletter" required  type="checkbox">
+                                            {{$t("newsletter_page.agree")}} {{property.name}}. 
+                                    </label>
+        					    </div>
+        					</div>
+        					<div class="form-group">
+                                <div class="col-xs-12">
+                                    <button class="contest_btn" type="submit" :disabled="formSuccess">{{$t("newsletter_page.subscribe")}}</button>
+                                </div>
+                            </div>
+                        </form>
                             
+                        <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
+                            <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                            <span class="sr-only">{{$t("newsletter_page.success")}} : </span>
+                            {{$t("newsletter_page.thank_you_message")}}
+                        </div>
+                        <div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">
+                            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                            <span class="sr-only">{{$t("newsletter_page.error")}} : </span>
+                            {{$t("newsletter_page.error_message")}}
                         </div>
                     </div>
                 </div>
