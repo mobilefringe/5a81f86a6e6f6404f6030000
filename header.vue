114f85dbf2aea7b786bed6322124e851617dadea
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-08 20:36:56 -0500

    Orchid Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -36,7 +36,7 @@
 				    <div class="col-md-2 hidden_phone"></div>
 					<div class="col-sm-12 col-md-8">
 					    <div class="nav_container">
-    					    <transition name="custom-classes-transition" enter-active-class="'animated' + { slideInDown: isMobile }" leave-active-class="'animated' + { slideOutUp: isMobile }">
+    					    <transition name="custom-classes-transition" enter-active-class="animated slideInDown" leave-active-class="animated slideOutUp">
         						<nav id="primary_nav" v-if="showMenu">
         							<ul>
         							    <li v-for="item in menu_items" class="menu_item" @click="toggleSubMenu(item.id)">
@@ -86,11 +86,11 @@
                 }
             },
             watch: {
-                // $route: function() {
-                //     if (this.windowWidth <= 768) {
-                //         this.showMenu = false;
-                //     }  
-                // },
+                $route: function() {
+                    if (this.windowWidth <= 768) {
+                        this.showMenu = false;
+                    }  
+                },
                 windowWidth: function() {
                     if (this.windowWidth <= 768) {
                         console.log(this.showMenu)
