ce195f342f3eec8c7ce3f84e02b31a192bae2775
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-12 11:34:19 -0400

    Orchid Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -58,7 +58,7 @@
                                         </div> 
                                         <div class="mobile_property_address center">
                                             <p>{{ property.name }}<br>
-                                                <a href="https://goo.gl/maps/RJ5dV8dxP1y" target="_blank">{{ property.address1 }} <br>{{ property.city }} {{ property.province_state }} {{ property.postal_code }}</a>
+                                                <a href="https://goo.gl/maps/RJ5dV8dxP1y" target="_blank">{{ property.address1 }}<br>{{ property.city }} {{ property.province_state }} {{ property.postal_code }}</a>
                                             </p>
                                             
                                         </div>
