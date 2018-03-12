3a3437e99b459cca2451b34445849fd33fa83a7c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-12 14:32:37 -0400

    Orchid Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -23,12 +23,9 @@
                         </div>
                     </div>
                     <div class="row">
-                        
-                        
                         <div class="col-md-9 col-md-push-3">
                             <!--<mapplic-map ref="mapplic_ref" :height="700" :minimap= "true" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList" tooltiplabel="Info"></mapplic-map>-->
                             Map goes here
-                            
                         </div>
                         <div class="margin-30 visible-mobile"></div>
                         <div class="col-md-3 col-md-pull-9">
