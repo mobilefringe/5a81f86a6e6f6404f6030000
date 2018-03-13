849b87f5e3e7ab1c6e8da88b3048e24f6a3d6f3a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-13 16:32:43 -0400

    Orchid Template 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -61,6 +61,9 @@
                             if(_.includes(this.currentEvent.image_url, 'missing')) {
                                 this.currentEvent.image_url = "http://via.placeholder.com/1560x800/757575";
                             }
+                            if(_.includes(this.currentEvent.image_url, 'missing')) {
+                                this.currentEvent.event_image2_url_abs = "http://via.placeholder.com/1560x800/757575";
+                            }
                         }
                     }
                 }
