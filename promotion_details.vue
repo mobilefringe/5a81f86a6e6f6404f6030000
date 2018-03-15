93f183833e778ef9fc6d2170863f8a081d9ade0d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-15 12:24:57 -0400

    Orchid Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -9,7 +9,8 @@
     					<div class="promo_container">
     					    
     					    <div class="promo_content center">
-    					        <p class="promo_title">{{ $t("promos_page.promotions") }}</p>
+    					        <p class="promo_title" v-if="promo.store">{{ currentPromo.store.name }}</p>
+        					        <p class="promo_title" v-else>{{ property.name }}</p>
     					        <h3 class="margin_60" v-if="locale=='en-ca'">{{ currentPromo.name }}</h3>
     							<h3 class="margin_60" v-else>{{ currentPromo.name_2 }}</h3>
     							<p class="promo_desc">
