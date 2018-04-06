435d2143cf105f493c45ccd02a369ef91b31e374
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-06 11:05:55 -0400

    Orchid Template 1.0  /twitter-slider.vue

--- twitter-slider.vue
+++ twitter-slider.vue
@@ -44,6 +44,31 @@
                         prevArrow: '.prev',
                         // slidesToShow: 3,
                         // speed: 1200
+                        responsive: [
+                            {
+                                breakpoint: 1024,
+                                settings: {
+                                    slidesToShow: 3,
+                                    slidesToScroll: 3,
+                                    infinite: true,
+                                    dots: true
+                                }
+                            },
+                            {
+                                breakpoint: 600,
+                                settings: {
+                                    slidesToShow: 2,
+                                    slidesToScroll: 2
+                                }
+                            },
+                            {
+                                breakpoint: 480,
+                                settings: {
+                                    slidesToShow: 1,
+                                    slidesToScroll: 1
+                                }
+                            }
+                        }
                     }
                 };
             },
