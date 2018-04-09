95379286f2be72ae30457f89a42f7f1fe3f79efb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-09 15:28:18 -0400

    Orchid Template 1.0  /instagram.vue

---
+++ instagram.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="dataLoaded" class="insta_feed_container">
+            
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vue!vue-slick"], function (Vue, slick) {
+        return Vue.component("twitter-slider", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    dataLoaded: false,
+                    instaFeed: null,
+                };
+            },
+            created () {
+                this.loadData().then(response => {
+                    var instaFeed = response[0].data;
+                    var insta_feed = instaFeed.social;
+                    this.instaFeed = _.slice(insta_feed, [0], [9]);
+                    
+                    this.dataLoaded = true;
+                });
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: "http://billingsbridge.mallmaverick.com/api/v3/billings/social.json"})]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
